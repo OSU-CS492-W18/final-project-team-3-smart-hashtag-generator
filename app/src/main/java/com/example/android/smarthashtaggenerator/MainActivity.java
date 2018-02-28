303e99727006f2f194131140b356acab9c027c85
wongnich <wongnich@gmail.com>
2018-02-27 16:42:26 -0800

    setting up buttons, and recycler view

--- app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.smarthashtaggenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTakePhotoBtn;
+    private Button mChoosePhotoBtn;
+    private Button mHistoryPhotoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTakePhotoBtn = findViewById(R.id.take_photo_btn);
+        mChoosePhotoBtn = findViewById(R.id.choose_photo_btn);
+        mHistoryPhotoBtn = findViewById(R.id.view_history_btn);
+
     }
+
+
 }
