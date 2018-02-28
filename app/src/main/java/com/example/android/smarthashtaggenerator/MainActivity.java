a08596c4426fddfb164190a3bb3dddd034d330e7
blazerzero <habibelo@oregonstate.edu>
2018-02-27 16:20:12 -0800

    Splash page finished. Added several background JPEGs for splash page and other pages. Made app open to splash page instead of main activity.

--- app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.smarthashtaggenerator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
