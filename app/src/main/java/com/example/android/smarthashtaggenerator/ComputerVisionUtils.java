c42e37a7108099cd89879965e91f7188780104da
Meg <olsenme@oregonstate.edu>
2018-03-04 16:27:58 -0800

    adding skeleton for the loader classes

---
+++ app/src/main/java/com/example/android/smarthashtaggenerator/ComputerVisionUtils.java
@@ -0,0 +1,11 @@
+package com.example.android.smarthashtaggenerator;
+
+/**
+ * Created by Owner on 3/4/2018.
+ */
+
+public class ComputerVisionUtils {
+
+    //final static String GITHUB_SEARCH_BASE_URL = "https://westus.api.cognitive.microsoft.com/vision/v1.0";
+
+}
