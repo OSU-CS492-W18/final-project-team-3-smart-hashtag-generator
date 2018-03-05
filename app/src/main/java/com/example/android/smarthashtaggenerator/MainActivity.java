c6a2efb8f05eb5446ef9a029e56b3b949a743d6b
blazerzero <habibelo@oregonstate.edu>
2018-03-05 12:42:44 -0800

    Set up OnClickListeners for MainActivity buttons. Initialized TakePhoto and ChoosePhoto activities.

--- app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.smarthashtaggenerator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
         mTakePhotoBtn = findViewById(R.id.take_photo_btn);
         mChoosePhotoBtn = findViewById(R.id.choose_photo_btn);
         mHistoryPhotoBtn = findViewById(R.id.view_history_btn);
+
+        mTakePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePhotoIntent = new Intent(MainActivity.this, TakePhoto.class);
+                startActivity(takePhotoIntent);
+            }
+        });
+
+        mChoosePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent choosePhotoIntent = new Intent(MainActivity.this, ChoosePhoto.class);
+                startActivity(choosePhotoIntent);
+            }
+        });
+
+        mHistoryPhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewHistoryIntent = new Intent(MainActivity.this, ViewHistory.class);
+                startActivity(viewHistoryIntent);
+            }
+        });
 
     }
 
