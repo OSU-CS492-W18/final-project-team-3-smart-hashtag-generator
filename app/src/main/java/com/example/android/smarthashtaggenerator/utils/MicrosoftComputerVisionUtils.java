72675f689e85ba8a62d47ace96db6c452f8dbb46
wongnich <wongnich@gmail.com>
2018-03-06 12:50:08 -0800

    building the url, adding headers to the network utils. Still need to pass the image into the url somehow

---
+++ app/src/main/java/com/example/android/smarthashtaggenerator/utils/MicrosoftComputerVisionUtils.java
@@ -0,0 +1,59 @@
+package com.example.android.smarthashtaggenerator.utils;
+
+
+import android.content.Context;
+import android.net.Uri;
+
+
+import com.example.android.smarthashtaggenerator.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+/**
+ * Created by wongnich on 3/6/18.
+ */
+
+public class MicrosoftComputerVisionUtils {
+
+    //final url: https://[location].api.cognitive.microsoft.com/vision/v1.0/analyze[?visualFeatures][&details][&language]
+    private final static String MIC_VISION__BASE_URL = "http://westus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+    private final static String MIC_VISION_VISUAL_FEATURE_PARAM = "?";
+
+    public static class ComputerVisionItem implements Serializable {
+        public static final String EXTRA_VISION_ITEM = "com.example.android.smarthashtaggenerator.utils.ComputerVisionItem.Result";
+        public String tag;
+    }
+
+    public static String buildVisionURL(/*will need picture here*/) {
+        return Uri.parse(MIC_VISION__BASE_URL).buildUpon()
+                .appendQueryParameter(MIC_VISION_VISUAL_FEATURE_PARAM, "Tags")
+                .build()
+                .toString();
+    }
+
+    public static ArrayList<ComputerVisionItem> parseForecastJSON(String forecastJSON) {
+        try {
+            //parse JSON Object
+
+            }
+            return ;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
--- app/src/main/java/com/example/android/smarthashtaggenerator/utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/utils/NetworkUtils.java
@@ -12,10 +12,22 @@ import okhttp3.Response;
 
 public class NetworkUtils {
 
+    private final static String MIC_VISION_APPID_PARAM = "Ocp-Apim-Subscription-Key";
+    private final static String MIC_VISION_CONTENT_TYPE_PARAM = "Content-Type";
+
+
+    private final static String MIC_VISION_CONTENT_TYPE = "application/octet-stream";
+    //API key expires March 16, 2018
+    private final static String MIC_VISION_APPID = "ec3bec6497eb490c8d2fa03e664660e2";
+
     private static final OkHttpClient mHTTPClient = new OkHttpClient();
+
+
 
     public static String doHTTPGet(String url) throws IOException {
         Request request = new Request.Builder()
+                .header(MIC_VISION_CONTENT_TYPE_PARAM, MIC_VISION_CONTENT_TYPE)
+                .header(MIC_VISION_APPID_PARAM, MIC_VISION_APPID)
                 .url(url)
                 .build();
         Response response = mHTTPClient.newCall(request).execute();
