e1da214de3fbc7ed4fb029684c15fc72539180e7
wongnich <wongnich@gmail.com>
2018-03-06 13:18:53 -0800

    updated network builder to build url with multipartbody.builder to include image

--- app/src/main/java/com/example/android/smarthashtaggenerator/utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/utils/NetworkUtils.java
@@ -1,9 +1,13 @@
 package com.example.android.smarthashtaggenerator.utils;
 
+import java.io.File;
 import java.io.IOException;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -24,11 +28,22 @@ public class NetworkUtils {
 
 
 
-    public static String doHTTPGet(String url) throws IOException {
+    public static String doHTTPGet(String url, File image) throws IOException {
+
+
+        //build the image into the url
+        RequestBody requestBody = new MultipartBody.Builder()
+                .addFormDataPart("file", image.getName(),
+                        RequestBody.create(MediaType.parse("application/octet-stream"), image))
+                .build();
+
+
+        //build the whole url
         Request request = new Request.Builder()
                 .header(MIC_VISION_CONTENT_TYPE_PARAM, MIC_VISION_CONTENT_TYPE)
                 .header(MIC_VISION_APPID_PARAM, MIC_VISION_APPID)
                 .url(url)
+                .post(requestBody)
                 .build();
         Response response = mHTTPClient.newCall(request).execute();
 
