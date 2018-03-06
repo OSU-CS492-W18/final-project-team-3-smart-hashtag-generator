a0b58f7a64423d4aa2f92dacd2fb0a6e8452f385
blazerzero <habibelo@oregonstate.edu>
2018-03-06 12:22:40 -0800

    User now able to take photo. App doesn't do anything with the photo yet.

---
+++ app/src/main/java/com/example/android/smarthashtaggenerator/Photo.java
@@ -0,0 +1,15 @@
+package com.example.android.smarthashtaggenerator;
+
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.Serializable;
+
+/**
+ * Created by Omeed on 3/5/18.
+ */
+
+public class Photo implements Serializable {
+    private ImageView iv;
+    private File file;
+}
--- app/src/main/java/com/example/android/smarthashtaggenerator/TakePhoto.java
+++ app/src/main/java/com/example/android/smarthashtaggenerator/TakePhoto.java
@@ -1,13 +1,132 @@
 package com.example.android.smarthashtaggenerator;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TakePhoto extends AppCompatActivity {
+
+    private static final String TAG = TakePhoto.class.getSimpleName();
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final String EXTRA_PHOTO = "TakePhoto";
+    private Uri mPhotoUri;
+    private File photoFile;
+    private Photo photo;
+    private ImageView imageView;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
+        /*Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            photoFile = this.createTemoraryFile("picture", ".jpg");
+            photoFile.delete();
+            mPhotoUri = Uri.fromFile(photoFile);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoUri);
+            startActivityForResult(cameraIntent, REQUEST_TAKE_PHOTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Unable to create file to take picture!");
+        }*/
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                try {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.example.android.smarthashtaggenerator.fileprovider",
+                            photoFile);
+
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    /*private File createTemoraryFile(String part, String ext) throws Exception {
+        File tempDir = Environment.getExternalStorageDirectory();
+        tempDir = new File(tempDir.getAbsolutePath() + "/.temp");
+        if (!tempDir.exists()) {
+            tempDir.mkdirs();
+        }
+        return File.createTempFile(part, ext, tempDir);
+    }
+
+    public void retrieveImage(ImageView imageView) {
+        this.getContentResolver().notifyChange(mPhotoUri, null);
+        ContentResolver cr = this.getContentResolver();
+        Bitmap photoBitMap;
+        try {
+            photoBitMap = MediaStore.Images.Media.getBitmap(cr, mPhotoUri);
+            imageView.setImageBitmap(photoBitMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "Failed to load photoFile", e);
+        }
+    }
+
+    /*public void capturePhoto(String targetFileName) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.withAppendedPath(mLocationForPhotos, targetFileName));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+        }
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            photo = new Photo();
+            //retrieveImage(imageView);
+            //Intent generateHashtagsIntent = new Intent(this, GenerateHashtags.class);
+            //generateHashtagsIntent.put
+        }
     }
 }
