586975d06eb7bb7af457313d8769bd7ad2749bc3
Benji Wick <benny.wick@gmail.com>
2018-03-16 16:22:22 -0700

    added adapter and such

---
+++ app/src/main/java/com/example/android/smarthashtaggenerator/Adapter.java
@@ -0,0 +1,10 @@
+package com.example.android.smarthashtaggenerator;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by benny on 3/16/2018.
+ */
+
+public class Adapter extends RecyclerView.Adapter {
+}
